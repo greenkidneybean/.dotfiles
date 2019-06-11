748a03f4c0a7dc02f360882851b5a515225a69b0
greenkidneybean <greenkidneybean@gmail.com>
2019-06-11 11:21:59 -0400

    setup script with backups and zsh plugin

---
+++ setup_dotfiles.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# backup ~/ dotfiles to ~/.dotfiles_backup 
+# symlink dotfiles/* to ~/
+
+echo "creating symlinks for project dotfiles..."
+pushd dotfiles > /dev/null 2>&1
+now=$(date +"%y%m%d.%H.%M")
+
+for file in .*; do
+  if [[ $file == "." || $file == ".." ]]; then
+    continue
+  fi
+  echo "running ~/$file"
+  # if the file exists:
+  if [[ -e ~/$file ]]; then
+      mkdir -p ~/.dotfiles_backup/$now
+      mv ~/$file ~/.dotfiles_backup/$now/$file
+      echo "backup saved as ~/.dotfiles_backup/$now/$file"
+  fi
+  # symlink might still exist
+  unlink ~/$file > /dev/null 2>&1
+  # create the link
+  ln -s ~/.dotfiles/dotfiles/$file ~/$file
+  echo '\tlinked'
+done
+
+popd > /dev/null 2>&1
+
+# install zsh plugins
+if [ -d ~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]
+then
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
